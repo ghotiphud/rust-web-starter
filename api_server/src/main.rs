24c03ff3c57e45771a9a7ca723d593ba41899326
Yale Cason <yale.cason@gmail.com>
2017-08-20 15:00:23 -0400

    Postgres Connection pooling

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -3,12 +3,30 @@
 
 extern crate rocket;
 
+extern crate lazy_static;
+extern crate dotenv;
+extern crate diesel;
+extern crate r2d2;
+extern crate r2d2_diesel;
+
+use dotenv::dotenv;
+use std::env;
+
+mod pg_pool;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
 fn main() {
+    // initialize env
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
     rocket::ignite()
+        .manage(pg_pool::init(&database_url))
         .mount("/api", routes![index]).launch();
 }
---
+++ api_server/src/pg_pool.rs
@@ -0,0 +1,43 @@
+use r2d2;
+use diesel::pg::PgConnection;
+use r2d2_diesel::ConnectionManager;
+
+type ManagedPgConn = ConnectionManager<PgConnection>;
+type Pool = r2d2::Pool<ManagedPgConn>;
+
+use std::ops::Deref;
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+/// Db Connection request guard type: wrapper around r2d2 pooled connection
+pub struct DbConn(pub r2d2::PooledConnection<ManagedPgConn>);
+
+/// Attempts to retrieve a single connection from the managed database pool. If
+/// no pool is currently managed, fails with an `InternalServerError` status. If
+/// no connections are available, fails with a `ServiceUnavailable` status.
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<DbConn, ()> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(DbConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+// For the convenience of using an &DbConn as an &SqliteConnection.
+impl Deref for DbConn {
+    type Target = PgConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub fn init(database_url: &str) -> Pool {
+    let config = r2d2::Config::default();
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    r2d2::Pool::new(config, manager).expect("Failed to create pool.")
+}
