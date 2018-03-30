b94f70d51b85255a8583694753018ee9e223ea99
michfarr <etnunc@protonmail.com>
2018-03-30 14:17:43 +0200

    Update to latest rocket and diesel version applicable

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -2,16 +2,18 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[allow(unused)]
 #[macro_use]
 extern crate rocket_contrib;
 
+#[allow(unused)]
 #[macro_use]
 extern crate serde_derive;
 
 #[macro_use]
 extern crate diesel;
 #[macro_use]
-extern crate diesel_codegen;
+extern crate diesel_infer_schema;
 extern crate dotenv;
 extern crate lazy_static;
 extern crate r2d2;
@@ -32,7 +34,6 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    // initialize env
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
